d31611280395dca0f195458f69bef42c72d2db2c
Chomii <milanche_show@hotmail.com>
2021-01-11 12:26:33 +0100

    fixed minor flake8 errors

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -31,7 +31,9 @@ class ModelTests(TestCase):
     def test_create_new_superuser(self):
         """Test creating new superuser """
 
-        user = get_user_model().objects.create_superuser('test@gmail.com', 'test12345')
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com', 'test12345'
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
